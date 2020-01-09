c6ff23f6877d6fa511046d337a31487654b8dd96
Guilherme Luiz <guiiluiz44@gmail.com>
2020-01-09 16:10:38 -0300

    test if shows only 1 pokemon

--- src/App.test.js
+++ src/App.test.js
@@ -22,3 +22,13 @@ test('shows the Pokedéx when the route is `/`', () => {
 
   expect(getByText('Encountered pokémons')).toBeInTheDocument();
 });
+
+test('the Pokedéx must show only 1 pokémon', () => {
+  const { queryAllByText } = render(
+    <MemoryRouter initialEntries={['/']}>
+      <App />
+    </MemoryRouter>,
+  );
+  const pokemon = queryAllByText(/Average weight:/i);
+  expect(pokemon.length).toBe(1);
+});
