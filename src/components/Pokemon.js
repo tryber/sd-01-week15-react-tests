d1eb93325af3821f220c07cedd087d01916ca01b
Henrique Eyer <henriqueeyer@outlook.com>
2020-01-09 18:33:36 -0300

    Alguns testes já realizados

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -15,14 +15,14 @@ class Pokemon extends React.Component {
     const { measurementUnit, value } = averageWeight;
 
     return (
-      <div className="pokemon">
+      <div data-testid={id} className="pokemon">
         <div className="pokemon-overview">
-          <p>{name}</p>
-          <p>{type}</p>
-          <p>{`Average weight: ${value} ${measurementUnit}`}</p>
+          <p data-testid="pokemon-name">{name}</p>
+          <p data-testid="pokemon-type">{type}</p>
+          <p data-testid="pokemon-weight">{`Average weight: ${value} ${measurementUnit}`}</p>
           {showDetailsLink && <Link to={`pokemons/${id}`}>More details</Link>}
         </div>
-        <img src={image} alt={`${name} sprite`} />
+        <img data-testid="pokemon-img" src={image} alt={`${name} sprite`} />
         {isFavorite && (
           <img
             className="favorite-icon"
