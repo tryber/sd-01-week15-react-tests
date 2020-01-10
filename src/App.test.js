26e912cfdb854a48526c46b37234ded2c9dca258
Guilherme Luiz <guiiluiz44@gmail.com>
2020-01-10 15:19:15 -0300

    test button to filter the pokedéx by the type of pokemon

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,117 @@ import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import { render, fireEvent } from '@testing-library/react';
 import App from './App';
-import data from './data';
+import { Pokedex } from './components';
+
+const pokemons = [
+  {
+    id: 1,
+    name: 'Pikachu',
+    type: 'Electric',
+    averageWeight: {
+      value: '6.0',
+      measurementUnit: 'kg',
+    },
+    image: 'https://cdn.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png',
+    moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Pikachu_(Pok%C3%A9mon)',
+    foundAt: [
+      {
+        location: 'Kanto Viridian Forest',
+        map: 'https://cdn.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png',
+      },
+      {
+        location: 'Kanto Power Plant',
+        map: 'https://cdn.bulbagarden.net/upload/b/bd/Kanto_Celadon_City_Map.png',
+      },
+    ],
+    summary: 'This intelligent Pokémon roasts hard berries with electricity to make them tender enough to eat.',
+  },
+  {
+    id: 2,
+    name: 'Charmander',
+    type: 'Fire',
+    averageWeight: {
+      value: '8.5',
+      measurementUnit: 'kg',
+    },
+    image: 'https://cdn.bulbagarden.net/upload/0/0a/Spr_5b_004.png',
+    moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Charmander_(Pok%C3%A9mon)',
+    foundAt: [
+      {
+        location: 'Alola Route 3',
+        map: 'https://cdn.bulbagarden.net/upload/9/93/Alola_Route_3_Map.png',
+      },
+      {
+        location: 'Kanto Route 3',
+        map: 'https://cdn.bulbagarden.net/upload/4/4a/Kanto_Route_3_Map.png',
+      },
+      {
+        location: 'Kanto Route 4',
+        map: 'https://cdn.bulbagarden.net/upload/2/24/Kanto_Route_4_Map.png',
+      },
+      {
+        location: 'Kanto Rock Tunnel',
+        map: 'https://cdn.bulbagarden.net/upload/6/6f/Kanto_Rock_Tunnel_Map.png',
+      },
+    ],
+    summary: 'The flame on its tail shows the strength of its life force. If it is weak, the flame also burns weakly.',
+  },
+  {
+    id: 3,
+    name: 'Caterpie',
+    type: 'Bug',
+    averageWeight: {
+      value: '2.9',
+      measurementUnit: 'kg',
+    },
+    image: 'https://cdn.bulbagarden.net/upload/8/83/Spr_5b_010.png',
+    moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Caterpie_(Pok%C3%A9mon)',
+    foundAt: [
+      {
+        location: 'Johto Route 30',
+        map: 'https://cdn.bulbagarden.net/upload/7/76/Johto_Route_30_Map.png',
+      },
+      {
+        location: 'Johto Route 31',
+        map: 'https://cdn.bulbagarden.net/upload/2/2b/Johto_Route_31_Map.png',
+      },
+      {
+        location: 'Ilex Forest',
+        map: 'https://cdn.bulbagarden.net/upload/a/ae/Johto_Ilex_Forest_Map.png',
+      },
+      {
+        location: 'Johto National Park',
+        map: 'https://cdn.bulbagarden.net/upload/4/4e/Johto_National_Park_Map.png',
+      },
+    ],
+    summary: 'For protection, it releases a horrible stench from the antennae on its head to drive away enemies.',
+  },
+  {
+    id: 4,
+    name: 'Ekans',
+    type: 'Poison',
+    averageWeight: {
+      value: '6.9',
+      measurementUnit: 'kg',
+    },
+    image: 'https://cdn.bulbagarden.net/upload/1/18/Spr_5b_023.png',
+    moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Ekans_(Pok%C3%A9mon)',
+    foundAt: [
+      {
+        location: 'Goldenrod Game Corner',
+        map: 'https://cdn.bulbagarden.net/upload/e/ec/Johto_Goldenrod_City_Map.png',
+      },
+    ],
+    summary: 'It can freely detach its jaw to swallow large prey whole. It can become too heavy to move, however.',
+  },
+];
+
+const isPokemonFavoriteById = {
+  1: true,
+  2: false,
+  3: false,
+  4: false,
+};
 
 test('renders a reading with the text `Pokédex`', () => {
   const { getByText } = render(
@@ -24,9 +134,9 @@ test('shows the Pokedéx when the route is `/`', () => {
   expect(getByText('Encountered pokémons')).toBeInTheDocument();
 });
 
-test('the Pokedéx must show only 1 pokémon', () => {
+test('Pokedéx should display only 1 pokémon', () => {
   const { queryAllByText } = render(
-    <MemoryRouter>
+    <MemoryRouter initialEntries={['/']}>
       <App />
     </MemoryRouter>,
   );
@@ -35,23 +145,41 @@ test('the Pokedéx must show only 1 pokémon', () => {
 });
 
 test('shows the next pókemon when press the button', () => {
-  const { getByText, getByTestId } = render(
-    <MemoryRouter>
-      <App />
+  const { getByText } = render(
+    <MemoryRouter initialEntries={['/']}>
+      <Pokedex pokemons={pokemons} isPokemonFavoriteById={isPokemonFavoriteById} />
     </MemoryRouter>,
   );
   const nextButton = getByText(/Próximo pokémon/i);
   expect(nextButton).toBeInTheDocument();
 
-  const firstPokemon = getByTestId('pokemon-name').textContent;
-
-  fireEvent.click(nextButton);
-
-  const pokemonName = getByTestId('pokemon-name');
-  expect(firstPokemon).not.toBe(pokemonName.textContent);
-
-  for (let i = 0; i < data.length - 1; i += 1) {
+  for (let i = 0; i < pokemons.length; i += 1) {
+    expect(getByText(pokemons[i].name)).toBeInTheDocument();
     fireEvent.click(nextButton);
   }
-  expect(pokemonName.textContent).toBe(firstPokemon);
+  expect(getByText(pokemons[0].name)).toBeInTheDocument();
+});
+
+test('filter the pokedéx by the type of pokemon', () => {
+  const { queryAllByText, getByText, getAllByText } = render(
+    <MemoryRouter initialEntries={['/']}>
+      <Pokedex pokemons={pokemons} isPokemonFavoriteById={isPokemonFavoriteById} />
+    </MemoryRouter>,
+  );
+
+  const pokemonTypes = [...new Set(pokemons.map((pokemon) => pokemon.type))];
+  const buttonAll = getByText(/All/i);
+  let aux = buttonAll;
+  for (let i = 0; i < pokemonTypes.length; i += 1) {
+    expect(aux.nextSibling.textContent).toBe(pokemonTypes[i]);
+    aux = aux.nextSibling;
+    if (queryAllByText(pokemonTypes[i]).length === 2) {
+      fireEvent.click(getAllByText(pokemonTypes[i])[1]);
+    } else {
+      fireEvent.click(getByText(pokemonTypes[i]));
+    }
+    expect(queryAllByText(pokemonTypes[i]).length).toBe(2);
+    fireEvent.click(getByText(/Próximo pokémon/i));
+    expect(getByText(/Average weight:/i).previousSibling.textContent).toBe(pokemonTypes[i]);
+  }
 });
