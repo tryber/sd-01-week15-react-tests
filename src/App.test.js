b2d939b450c011a7d28098898b962c26ab01a43d
Victor Felipe <vf.ramos7@gmail.com>
2020-01-10 16:04:02 -0300

    Finalizado o requisito 18

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import App from './App';
 
 test('renders a reading with the text `Pokédex`', () => {
@@ -13,12 +13,37 @@ test('renders a reading with the text `Pokédex`', () => {
   expect(heading).toBeInTheDocument();
 });
 
+//1
 test('shows the Pokedéx when the route is `/`', () => {
   const { getByText } = render(
     <MemoryRouter initialEntries={['/']}>
       <App />
     </MemoryRouter>,
   );
-
   expect(getByText('Encountered pokémons')).toBeInTheDocument();
+});
+
+//2
+test('A Pokédex deve exibir apenas um pokémon por vez', () => {
+  const { queryAllByText } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+  const onePokemon = queryAllByText(/Average weight/i);
+  expect(onePokemon.length).toBe(1);
+});
+
+//18
+test('Ao clicar no link "Home" na barra de navegação, a aplicação deve ser redirecionada para a página inicial, na URL "/"', () => {
+  const { getByText } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>
+  );
+
+  fireEvent.click(getByText(/Home/i));
+
+  const homeInfo = getByText(/Próximo pokémon/i);
+  expect(homeInfo).toBeInTheDocument();
 });
