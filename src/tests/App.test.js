4b63f612271a57d0aaa1de7c09275d50ab483974
dougfunny1983 <douglaseduardo220483@gmail.com>
2020-01-10 18:10:18 -0300

    requisito 3 e 4 feitos...

---
+++ src/tests/App.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { render } from '@testing-library/react';
+import App from '../App';
+
+test('renders a reading with the text `Pokédex`', () => {
+  const { getByText } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+  const heading = getByText(/Pokédex/i);
+  expect(heading).toBeInTheDocument();
+});
+test('1 - shows the Pokedéx when the route is `/`', () => {
+  const { getByText } = render(
+    <MemoryRouter initialEntries={['/']}>
+      <App />
+    </MemoryRouter>,
+  );
+
+  expect(getByText('Encountered pokémons')).toBeInTheDocument();
+});
