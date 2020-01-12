0afbc2d1601b00cb0dd042f33a026969a6a750a6
Anderson Bolivar <bolivar.anderson@hotmail.com>
2020-01-12 12:03:26 -0200

    Third.One Tesk

--- src/components/Pokedex.test.js
+++ src/components/Pokedex.test.js
@@ -1,8 +1,12 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, cleanup } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import Pokedex from './Pokedex';
 import MockTest, { pokemons_Mock, isPokemonFavoriteById_Mock } from '../MockTests/MockTest';
+
+
+
+const pokeName = pokemons_Mock.map((pokemon => pokemon.name));
 
 test('expect the Pokédex only shows one pokémon at the time', () => {
   const { queryAllByText } = render(
@@ -11,7 +15,18 @@ test('expect the Pokédex only shows one pokémon at the time', () => {
     </MemoryRouter>
   );
 
-  expect(queryAllByText(/Ditto/i).length).toBe(1);
-  expect(queryAllByText(/Charmander/i).length).toBe(0);
-  expect(queryAllByText(/Jigglypuff/i).length).toBe(0);
+  pokeName.forEach(pokemon => {
+    pokemon === pokeName[0] ? expect(queryAllByText(pokemon).length).toBe(1)
+      : expect(queryAllByText(pokemon).length).toBe(0);
+  });
+});
+
+test('button label must be "Próximo pokémon"', () => {
+  const { getByText } = render(
+    <MemoryRouter initialEntries={['/']}>
+      <Pokedex pokemons={pokemons_Mock} isPokemonFavoriteById={isPokemonFavoriteById_Mock} />
+    </MemoryRouter>
+  );
+
+  expect(getByText(/Próximo pokémon/i)).toHaveTextContent('Próximo pokémon');
 });
