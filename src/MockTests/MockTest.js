4263749ce793d6cfd2aa32420716000d4af7c41d
Anderson Bolivar <bolivar.anderson@hotmail.com>
2020-01-12 14:00:48 -0200

    Forth.One Tesk

--- src/MockTests/MockTest.js
+++ src/MockTests/MockTest.js
@@ -54,7 +54,7 @@ export const pokemonsMock = [
   {
     id: 3,
     name: 'Jigglypuff',
-    type: 'Nomral',
+    type: 'Normal',
     averageWeight: {
       value: '5.5',
       measurementUnit: 'kg',
@@ -73,10 +73,71 @@ export const pokemonsMock = [
     ],
     summary: 'For protection, it releases a horrible stench from the antennae on its head to drive away enemies.',
   },
+  {
+    id: 4,
+    name: 'Lapras',
+    type: 'Water',
+    averageWeight: {
+      value: '200.0',
+      measurementUnit: 'kg',
+    },
+    image: 'https://archives.bulbagarden.net/media/upload/6/69/Spr_4h_131.png',
+    moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Lapras_(Pokémon)',
+    foundAt: [
+      {
+        location: 'Silph Co',
+        map: 'https://cdn.bulbagarden.net/upload/6/6a/Kanto_Saffron_City_Map.png',
+      },
+    ],
+    summary: 'A gentle soul that can read the minds of people. It can ferry people across the sea on its back.',
+  },
+  {
+    id: 5,
+    name: 'Squirtle',
+    type: 'Water',
+    averageWeight: {
+      value: '9.0',
+      measurementUnit: 'kg',
+    },
+    image: 'https://archives.bulbagarden.net/media/upload/d/d6/Spr_4d_007_s.png',
+    moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Squirtle_(Pok%C3%A9mon)',
+    foundAt: [
+      {
+        location: 'Vermilion City',
+        map: 'https://cdn.bulbagarden.net/upload/5/54/Kanto_Vermilion_City_Map.png',
+      },
+    ],
+    summary: 'Shoots water at prey while in the water. Withdraws into its shell when in danger.',
+  },
+  {
+    id: 6,
+    name: 'Staryu',
+    type: 'Water',
+    averageWeight: {
+      value: '34.0',
+      measurementUnit: 'kg',
+    },
+    image: 'https://archives.bulbagarden.net/media/upload/d/d9/Spr_3e_120.png',
+    moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Staryu_(Pok%C3%A9mon)',
+    foundAt: [
+      {
+        location: 'Route 19',
+        map: 'https://cdn.bulbagarden.net/upload/5/51/Kanto_Route_19_Map.png',
+      },
+      {
+        location: 'Route 21',
+        map: 'https://cdn.bulbagarden.net/upload/e/e9/Kanto_Route_21_Map.png',
+      },
+    ],
+    summary: '	As long as the center section is unharmed, it can grow back fully even if it is chopped to bits.',
+  },
 ];
 
 export const isPokemonFavoriteByIdMock = {
   1: false,
   2: false,
   3: false,
+  4: false,
+  5: true,
+  6: false,
 };
--- src/components/Pokedex.test.js
+++ src/components/Pokedex.test.js
@@ -1,64 +1,99 @@
 import React from 'react';
-import { render, cleanup, fireEvent } from '@testing-library/react';
+import { render, cleanup, fireEvent, getAllByText } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import Pokedex from './Pokedex';
 import MockTest, { pokemonsMock, isPokemonFavoriteByIdMock } from '../MockTests/MockTest';
 
 afterEach(cleanup);
 
-const pokeName = pokemonsMock.map((pokemon => pokemon.name));
+const pokeName = pokemonsMock.map(pokemon => pokemon.name);
+const pokeTypes = pokemonsMock.map(pokemon => pokemon.type);
+const pokeTypeFilter = pokeTypes.filter((pokemon, index) => pokeTypes.indexOf(pokemon) === index);
 
-test('expect the Pokédex only shows one pokémon at the time', () => {
-  const { queryAllByText } = render(
-    <MemoryRouter initialEntries={['/']}>
-      <Pokedex pokemons={pokemonsMock} isPokemonFavoriteById={isPokemonFavoriteByIdMock} />
-    </MemoryRouter>
-  );
+describe('Pokédex pokémon check', () => {
+  test('expect the Pokédex only shows one pokémon at the time', () => {
+    const { queryAllByText } = render(
+      <MemoryRouter initialEntries={['/']}>
+        <Pokedex pokemons={pokemonsMock} isPokemonFavoriteById={isPokemonFavoriteByIdMock} />
+      </MemoryRouter>
+    );
 
-  pokeName.forEach(pokemon => {
-    pokemon === pokeName[0] ? expect(queryAllByText(pokemon).length).toBe(1)
-      : expect(queryAllByText(pokemon).length).toBe(0);
+    pokeName.forEach(pokemon => {
+      pokemon === pokeName[0] ? expect(queryAllByText(pokemon).length).toBe(1)
+        : expect(queryAllByText(pokemon).length).toBe(0);
+    });
   });
-});
+})
 
-test('button label must be "Próximo pokémon"', () => {
-  const { getByText } = render(
-    <MemoryRouter initialEntries={['/']}>
-      <Pokedex pokemons={pokemonsMock} isPokemonFavoriteById={isPokemonFavoriteByIdMock} />
-    </MemoryRouter>
-  );
+describe('Pokédex button "Próximo pokémon"', () => {
+  test('button label must be "Próximo pokémon"', () => {
+    const { getByText } = render(
+      <MemoryRouter initialEntries={['/']}>
+        <Pokedex pokemons={pokemonsMock} isPokemonFavoriteById={isPokemonFavoriteByIdMock} />
+      </MemoryRouter>
+    );
 
-  expect(getByText(/Próximo pokémon/i)).toHaveTextContent('Próximo pokémon');
-});
-
-test('when button is clicked must display the others pokémons', () => {
-  const { getByText, queryAllByText } = render(
-    <MemoryRouter initialEntries={['/']}>
-      <Pokedex pokemons={pokemonsMock} isPokemonFavoriteById={isPokemonFavoriteByIdMock} />
-    </MemoryRouter>
-  );
-
-  pokeName.forEach((pokemon, index) => {
-    pokemon === pokeName[index] ? expect(queryAllByText(pokemon).length).toBe(1)
-      : expect(queryAllByText(pokemon).length).toBe(0);
-
-    fireEvent.click(getByText(/Próximo pokémon/i));
-  });
-});
-
-test('after gets in the last pokémon, when click the button again must show the first pokémon', () => {
-  const { getByText, queryAllByText } = render(
-    <MemoryRouter initialEntries={['/']}>
-      <Pokedex pokemons={pokemonsMock} isPokemonFavoriteById={isPokemonFavoriteByIdMock} />
-    </MemoryRouter>
-  );
-
-  pokeName.forEach(() => {
-    fireEvent.click(getByText(/Próximo pokémon/i));
+    expect(getByText(/Próximo pokémon/i)).toHaveTextContent('Próximo pokémon');
   });
 
-  pokeName.forEach(pokemon => {
-    pokemon === pokeName[0] ? expect(queryAllByText(pokemon).length).toBe(1)
-      : expect(queryAllByText(pokemon).length).toBe(0);
+  test('when button is clicked must display the others pokémons', () => {
+    const { getByText, queryAllByText } = render(
+      <MemoryRouter initialEntries={['/']}>
+        <Pokedex pokemons={pokemonsMock} isPokemonFavoriteById={isPokemonFavoriteByIdMock} />
+      </MemoryRouter>
+    );
+
+    pokeName.forEach((pokemon, index) => {
+      pokemon === pokeName[index] ? expect(queryAllByText(pokemon).length).toBe(1)
+        : expect(queryAllByText(pokemon).length).toBe(0);
+
+      fireEvent.click(getByText(/Próximo pokémon/i));
+    });
   });
-});
+
+  test('after gets in the last pokémon, when click the button again must show the first pokémon', () => {
+    const { getByText, queryAllByText } = render(
+      <MemoryRouter initialEntries={['/']}>
+        <Pokedex pokemons={pokemonsMock} isPokemonFavoriteById={isPokemonFavoriteByIdMock} />
+      </MemoryRouter>
+    );
+
+    pokeName.forEach(() => {
+      fireEvent.click(getByText(/Próximo pokémon/i));
+    });
+
+    pokeName.forEach(pokemon => {
+      pokemon === pokeName[0] ? expect(queryAllByText(pokemon).length).toBe(1)
+        : expect(queryAllByText(pokemon).length).toBe(0);
+    });
+  });
+})
+
+describe('Pokédex filter type buttons', () => {
+  test('when click a filter type button only pokémons that type must appear', () => {
+    const { debug, getByText, getAllByRole, queryAllByText } = render(
+      <MemoryRouter initialEntries={['/']}>
+        <Pokedex pokemons={pokemonsMock} isPokemonFavoriteById={isPokemonFavoriteByIdMock} />
+      </MemoryRouter>
+    );
+
+    pokeTypeFilter.forEach(type => {
+      const typeButton = queryAllByText(type)
+        .find(elm => {
+          if (getAllByRole('button').find(button => button === elm) !== undefined) {
+            return true
+          } return false
+        });
+
+      fireEvent.click(typeButton);
+
+      const typePokemons = pokemonsMock.filter(pokemon => pokemon.type === type).map(pokemon => pokemon.name);
+      typePokemons.forEach((pokemon, index) => {
+        pokemon === typePokemons[index] ? expect(queryAllByText(pokemon).length).toBe(1)
+          : expect(queryAllByText(pokemon).length).toBe(0);
+
+        fireEvent.click(getByText(/Próximo pokémon/i));
+      });
+    });
+  });
+})
