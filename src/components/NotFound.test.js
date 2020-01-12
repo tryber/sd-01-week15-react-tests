f1aad623bf7eea0452cacbbfc4c347ea50e6b8f8
Henrique Eyer <henriqueeyer@outlook.com>
2020-01-12 17:13:15 -0300

    FAlta poucos

---
+++ src/components/NotFound.test.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
+import { Router } from 'react-router-dom';
+import NotFound from './NotFound';
+
+function renderWithRouter(
+  ui,
+  { route = '/', history = createMemoryHistory({ initialEntries: [route] }) } = {},
+) {
+  return {
+    ...render(<Router history={history}>{ui}</Router>),
+    history,
+  };
+}
+
+test('23 - Entrar em uma URL desconhecida exibe a página Not Found', () => {
+  const { getByText, getByAltText } = renderWithRouter(
+    <NotFound />, { route: '/coruja' }
+  );
+  expect(getByText('Page requested not found')).toBeInTheDocument();
+  expect(getByText('Page requested not found').tagName).toBe('H2');
+  const imgError = getByAltText('Pikachu crying because the page requested was not found');
+  expect(imgError).toBeInTheDocument();
+  // expect(imgError.src).toBe('https://testing-library.com/');
+});
