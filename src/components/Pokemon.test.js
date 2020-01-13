09548fca1cc3c7f462be42781109e7300c6d5a73
Mateus Talles <mateustalles@gmail.com>
2020-01-12 23:56:34 -0300

    Ex. 8 done

---
+++ src/components/Pokemon.test.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { render, getByText, fireEvent, container } from '@testing-library/react';
+import Pokemon from './Pokemon';
+
+describe('new test case', () => {
+  it('8 - poke characteristics', () => {
+    const pokemon = {
+      id: 25,
+      name: 'Pikachu',
+      type: 'Electric',
+      averageWeight: {
+        value: '6.0',
+        measurementUnit: 'kg',
+      },
+      image: 'https://cdn.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png',
+      moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Pikachu_(Pok%C3%A9mon)',
+      foundAt: [
+        {
+          location: 'Kanto Viridian Forest',
+          map: 'https://cdn.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png',
+        },
+        {
+          location: 'Kanto Power Plant',
+          map: 'https://cdn.bulbagarden.net/upload/b/bd/Kanto_Celadon_City_Map.png',
+        },
+      ],
+      summary: 'This intelligent Pokémon roasts hard berries with electricity to make them tender enough to eat.',
+    };
+
+    const isFavorite = true;
+
+    const { getByText, container } = render(
+      <MemoryRouter>
+        <Pokemon pokemon={pokemon} isFavorite={isFavorite} />
+      </MemoryRouter>,
+    );
+
+    const { averageWeight: { measurementUnit, value } } = pokemon;
+    const avgWeight = getByText(/Average weight/i);
+    expect(avgWeight.innerHTML).toBe(`Average weight: ${value} ${measurementUnit}`);
+
+    // Second pre-requisite of the test: images
+    const pokeImg = container.querySelector('img');
+    expect(pokeImg).toHaveProperty('src', pokemon.image);
+    expect(pokeImg).toHaveProperty('alt', `${pokemon.name} sprite`);
+  });
+});
