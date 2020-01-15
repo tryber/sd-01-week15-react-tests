49b3d657156eda1a24dede7a2f1e596d19d9b0a6
antoniopaolillo <antonio_francisco_p@live.com>
2020-01-15 19:59:59 -0300

    upando

---
+++ src/components/PokemonDetails.test.js
@@ -0,0 +1,16 @@
+// import React from "react";
+// import { MemoryRouter, Router } from "react-router-dom";
+// import { createMemoryHistory } from 'history';
+// import { render, fireEvent, cleanup } from "@testing-library/react";
+// import PokemonDetails from "./PokemonDetails";
+
+// const 
+// describe('11 - A página de detalhes de pokémon deve exibir o nome, tipo, peso médio e imagem do pokémon exibido', () => {
+//     test('O peso médio do pokémon deve ser exibido com um texto no formato Average weight: <value> <measurementUnit>, onde <value> e <measurementUnit> são, respectivamente, o peso médio do pokémon e sua unidade de medida', () => {
+  
+//     })
+  
+//     test('', () => {
+      
+//     })
+//   })
