c90aea0aa869a403dcc478d7f3d02332a122255f
antoniopaolillo <antonio_francisco_p@live.com>
2020-01-16 12:05:28 -0300

    formatando documentos

--- src/App.test.js
+++ src/App.test.js
@@ -1,199 +1,209 @@
-import React from 'react';
-import App from './App';
-import { MemoryRouter, Router } from 'react-router-dom';
-import { createMemoryHistory } from 'history';
-import { render, fireEvent, cleanup } from '@testing-library/react';
+import React from "react";
+import App from "./App";
+import { MemoryRouter, Router } from "react-router-dom";
+import { createMemoryHistory } from "history";
+import { render, fireEvent, cleanup } from "@testing-library/react";
 // import Pokedex from './components/Pokedex';
 
 function renderWithRouter(
   ui,
-  { route = '/', history = createMemoryHistory({ initialEntries: [route] }) } = {},
+  {
+    route = "/",
+    history = createMemoryHistory({ initialEntries: [route] })
+  } = {}
 ) {
   return {
     ...render(<Router history={history}>{ui}</Router>),
-    history,
+    history
   };
 }
 const isPokemonFavoriteById = {
   4: false,
   10: true,
   23: false,
-  78: true,
+  78: true
 };
 
 const pokemons = [
   {
     id: 4,
-    name: 'Charmander',
-    type: 'Fire',
+    name: "Charmander",
+    type: "Fire",
     averageWeight: {
-      value: '8.5',
-      measurementUnit: 'kg',
+      value: "8.5",
+      measurementUnit: "kg"
     },
-    image: 'https://cdn.bulbagarden.net/upload/0/0a/Spr_5b_004.png',
-    moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Charmander_(Pok%C3%A9mon)',
+    image: "https://cdn.bulbagarden.net/upload/0/0a/Spr_5b_004.png",
+    moreInfo:
+      "https://bulbapedia.bulbagarden.net/wiki/Charmander_(Pok%C3%A9mon)",
     foundAt: [
       {
-        location: 'Alola Route 3',
-        map: 'https://cdn.bulbagarden.net/upload/9/93/Alola_Route_3_Map.png',
+        location: "Alola Route 3",
+        map: "https://cdn.bulbagarden.net/upload/9/93/Alola_Route_3_Map.png"
       },
       {
-        location: 'Kanto Route 3',
-        map: 'https://cdn.bulbagarden.net/upload/4/4a/Kanto_Route_3_Map.png',
+        location: "Kanto Route 3",
+        map: "https://cdn.bulbagarden.net/upload/4/4a/Kanto_Route_3_Map.png"
       },
       {
-        location: 'Kanto Route 4',
-        map: 'https://cdn.bulbagarden.net/upload/2/24/Kanto_Route_4_Map.png',
+        location: "Kanto Route 4",
+        map: "https://cdn.bulbagarden.net/upload/2/24/Kanto_Route_4_Map.png"
       },
       {
-        location: 'Kanto Rock Tunnel',
-        map: 'https://cdn.bulbagarden.net/upload/6/6f/Kanto_Rock_Tunnel_Map.png',
-      },
+        location: "Kanto Rock Tunnel",
+        map: "https://cdn.bulbagarden.net/upload/6/6f/Kanto_Rock_Tunnel_Map.png"
+      }
     ],
-    summary: 'The flame on its tail shows the strength of its life force. If it is weak, the flame also burns weakly.',
+    summary:
+      "The flame on its tail shows the strength of its life force. If it is weak, the flame also burns weakly."
   },
   {
     id: 10,
-    name: 'Caterpie',
-    type: 'Bug',
+    name: "Caterpie",
+    type: "Bug",
     averageWeight: {
-      value: '2.9',
-      measurementUnit: 'kg',
+      value: "2.9",
+      measurementUnit: "kg"
     },
-    image: 'https://cdn.bulbagarden.net/upload/8/83/Spr_5b_010.png',
-    moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Caterpie_(Pok%C3%A9mon)',
+    image: "https://cdn.bulbagarden.net/upload/8/83/Spr_5b_010.png",
+    moreInfo: "https://bulbapedia.bulbagarden.net/wiki/Caterpie_(Pok%C3%A9mon)",
     foundAt: [
       {
-        location: 'Johto Route 30',
-        map: 'https://cdn.bulbagarden.net/upload/7/76/Johto_Route_30_Map.png',
+        location: "Johto Route 30",
+        map: "https://cdn.bulbagarden.net/upload/7/76/Johto_Route_30_Map.png"
       },
       {
-        location: 'Johto Route 31',
-        map: 'https://cdn.bulbagarden.net/upload/2/2b/Johto_Route_31_Map.png',
+        location: "Johto Route 31",
+        map: "https://cdn.bulbagarden.net/upload/2/2b/Johto_Route_31_Map.png"
       },
       {
-        location: 'Ilex Forest',
-        map: 'https://cdn.bulbagarden.net/upload/a/ae/Johto_Ilex_Forest_Map.png',
+        location: "Ilex Forest",
+        map: "https://cdn.bulbagarden.net/upload/a/ae/Johto_Ilex_Forest_Map.png"
       },
       {
-        location: 'Johto National Park',
-        map: 'https://cdn.bulbagarden.net/upload/4/4e/Johto_National_Park_Map.png',
-      },
+        location: "Johto National Park",
+        map:
+          "https://cdn.bulbagarden.net/upload/4/4e/Johto_National_Park_Map.png"
+      }
     ],
-    summary: 'For protection, it releases a horrible stench from the antennae on its head to drive away enemies.',
+    summary:
+      "For protection, it releases a horrible stench from the antennae on its head to drive away enemies."
   },
   {
     id: 23,
-    name: 'Ekans',
-    type: 'Poison',
+    name: "Ekans",
+    type: "Poison",
     averageWeight: {
-      value: '6.9',
-      measurementUnit: 'kg',
+      value: "6.9",
+      measurementUnit: "kg"
     },
-    image: 'https://cdn.bulbagarden.net/upload/1/18/Spr_5b_023.png',
-    moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Ekans_(Pok%C3%A9mon)',
+    image: "https://cdn.bulbagarden.net/upload/1/18/Spr_5b_023.png",
+    moreInfo: "https://bulbapedia.bulbagarden.net/wiki/Ekans_(Pok%C3%A9mon)",
     foundAt: [
       {
-        location: 'Goldenrod Game Corner',
-        map: 'https://cdn.bulbagarden.net/upload/e/ec/Johto_Goldenrod_City_Map.png',
-      },
+        location: "Goldenrod Game Corner",
+        map:
+          "https://cdn.bulbagarden.net/upload/e/ec/Johto_Goldenrod_City_Map.png"
+      }
     ],
-    summary: 'It can freely detach its jaw to swallow large prey whole. It can become too heavy to move, however.',
+    summary:
+      "It can freely detach its jaw to swallow large prey whole. It can become too heavy to move, however."
   },
   {
     id: 78,
-    name: 'Rapidash',
-    type: 'Fire',
+    name: "Rapidash",
+    type: "Fire",
     averageWeight: {
-      value: '95.0',
-      measurementUnit: 'kg',
+      value: "95.0",
+      measurementUnit: "kg"
     },
-    image: 'https://cdn.bulbagarden.net/upload/5/58/Spr_5b_078.png',
-    moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Rapidash_(Pok%C3%A9mon)',
+    image: "https://cdn.bulbagarden.net/upload/5/58/Spr_5b_078.png",
+    moreInfo: "https://bulbapedia.bulbagarden.net/wiki/Rapidash_(Pok%C3%A9mon)",
     foundAt: [
       {
-        location: 'Kanto Route 28',
-        map: 'https://cdn.bulbagarden.net/upload/5/5b/Kanto_Route_28_Map.png',
+        location: "Kanto Route 28",
+        map: "https://cdn.bulbagarden.net/upload/5/5b/Kanto_Route_28_Map.png"
       },
       {
-        location: 'Johto Mount Silver',
-        map: 'https://cdn.bulbagarden.net/upload/9/95/Johto_Mt_Silver_Map.png',
-      },
+        location: "Johto Mount Silver",
+        map: "https://cdn.bulbagarden.net/upload/9/95/Johto_Mt_Silver_Map.png"
+      }
     ],
-    summary: 'At full gallop, its four hooves barely touch the ground because it moves so incredibly fast.',
-  },
+    summary:
+      "At full gallop, its four hooves barely touch the ground because it moves so incredibly fast."
+  }
 ];
 
-test('renders a reading with the text `Pokédex`', () => {
+test("renders a reading with the text `Pokédex`", () => {
   const { getByText } = render(
     <MemoryRouter>
       <App />
-    </MemoryRouter>,
+    </MemoryRouter>
   );
   const heading = getByText(/Pokédex/i);
   expect(heading).toBeInTheDocument();
 });
-test('shows the Pokedéx when the route is `/`', () => {
+test("shows the Pokedéx when the route is `/`", () => {
   const { getByText } = render(
-    <MemoryRouter initialEntries={['/']}>
+    <MemoryRouter initialEntries={["/"]}>
       <App />
-    </MemoryRouter>,
+    </MemoryRouter>
   );
 
-  expect(getByText('Encountered pokémons')).toBeInTheDocument();
+  expect(getByText("Encountered pokémons")).toBeInTheDocument();
 });
 
-describe('17 - No topo da aplicação, deve haver um conjunto fixo de links de navegação', () => {
-  test('verificando os nomes e rotas dos links', () => {
+describe("17 - No topo da aplicação, deve haver um conjunto fixo de links de navegação", () => {
+  test("verificando os nomes e rotas dos links", () => {
     const { getByText } = render(
-      <MemoryRouter initialEntries={['/']}>
+      <MemoryRouter initialEntries={["/"]}>
         <App />
-      </MemoryRouter>,
+      </MemoryRouter>
     );
-    expect(getByText('Home').innerHTML).toBe('Home');
-    expect(getByText('Home').href).toBe('http://localhost/');
-    
-    expect(getByText('About').innerHTML).toBe('About');
-    expect(getByText('About').href).toBe('http://localhost/about');
-  
-    expect(getByText('Favorite Pokémons').innerHTML).toBe('Favorite Pokémons');
-    expect(getByText('Favorite Pokémons').href).toBe('http://localhost/favorites');
+    expect(getByText("Home").innerHTML).toBe("Home");
+    expect(getByText("Home").href).toBe("http://localhost/");
+
+    expect(getByText("About").innerHTML).toBe("About");
+    expect(getByText("About").href).toBe("http://localhost/about");
+
+    expect(getByText("Favorite Pokémons").innerHTML).toBe("Favorite Pokémons");
+    expect(getByText("Favorite Pokémons").href).toBe(
+      "http://localhost/favorites"
+    );
   });
 });
 
 describe('18 - Ao clicar no link "Home" na barra de navegação, a aplicação deve ser redirecionada para a página inicial, na URL "/"', () => {
-  test('verificando link e direcionamento', () => {
-    const {getByText, history} = renderWithRouter(
-      <App />
-    );
-    expect(history.location.pathname).toBe('/');
-    const linkHome = getByText('Home');
+  test("verificando link e direcionamento", () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    expect(history.location.pathname).toBe("/");
+    const linkHome = getByText("Home");
     fireEvent.click(linkHome);
-    expect(`http://localhost${history.location.pathname}`).toBe('http://localhost/');
-  })
-})
+    expect(`http://localhost${history.location.pathname}`).toBe(
+      "http://localhost/"
+    );
+  });
+});
 
 describe('19 - Ao clicar no link "About" na barra de navegação, a aplicação deve ser redirecionada para a página de `About`, na URL "/about"', () => {
-  test('verificando link e direcionamento', () => {
-    const {getByText, history} = renderWithRouter(
-      <App />
-    );
-    expect(history.location.pathname).toBe('/');
-    const linkAbout = getByText('About');
+  test("verificando link e direcionamento", () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    expect(history.location.pathname).toBe("/");
+    const linkAbout = getByText("About");
     fireEvent.click(linkAbout);
-    expect(`http://localhost${history.location.pathname}`).toBe('http://localhost/about');
-  })
-})
-describe('20 - Ao clicar no link "Favorite Pokémons" na barra de navegação, a aplicação deve ser redirecionada para a página de pokémons favoritados, na URL "/favorites"', () => {
-  test('verificando link e direcionamento', () => {
-    const {getByText, history} = renderWithRouter(
-      <App />
+    expect(`http://localhost${history.location.pathname}`).toBe(
+      "http://localhost/about"
     );
-    expect(history.location.pathname).toBe('/');
-    const linkFavorite = getByText('Favorite Pokémons');
+  });
+});
+describe('20 - Ao clicar no link "Favorite Pokémons" na barra de navegação, a aplicação deve ser redirecionada para a página de pokémons favoritados, na URL "/favorites"', () => {
+  test("verificando link e direcionamento", () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    expect(history.location.pathname).toBe("/");
+    const linkFavorite = getByText("Favorite Pokémons");
     fireEvent.click(linkFavorite);
-    expect(`http://localhost${history.location.pathname}`).toBe('http://localhost/favorites');
-  })
-})
-
-
+    expect(`http://localhost${history.location.pathname}`).toBe(
+      "http://localhost/favorites"
+    );
+  });
+});
