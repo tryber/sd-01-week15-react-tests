ea997efeefc398c993affe5b714b2049181b54de
Douglas Eduardo <douglaseduardo220483@gmail.com>
2020-02-08 21:00:44 -0300

    Terminado o 18. O 19 e 20 vai ser moleza

--- src/tests/Requirement-18.test.js
+++ src/tests/Requirement-18.test.js
@@ -1,17 +1,27 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
 import { render, fireEvent } from '@testing-library/react';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
 import App from '../App';
+
+function renderWithRouter(
+  ui,
+  { route = '/', history = createMemoryHistory({ initialEntries: [route] }) } = {},
+) {
+  return {
+    ...render(<Router history={history}>{ui}</Router>),
+    history,
+  };
+}
 
 describe('Exigência → 18', () => {
   test(`Ao clicar no link "Home" na barra de navegação, a aplicação
    deve ser redirecionada para a página inicial, na URL "/"`, () => {
-    const { getByText } = render(
-      <MemoryRouter initialEntries={['/']}>
-        <App />
-      </MemoryRouter>,
-    );
-    fireEvent.click(getByText('Home'));
-    expect(getByText('Encountered pokémons')).toBeInTheDocument();
+    const { getByText, history } = renderWithRouter(<App />);
+    expect(history.location.pathname).toBe('/');
+    const btnHome = getByText(/Home/i);
+    const home = btnHome.href;
+    fireEvent.click(btnHome);
+    expect(`http://localhost${history.location.pathname}`).toBe(home);
   });
 });
